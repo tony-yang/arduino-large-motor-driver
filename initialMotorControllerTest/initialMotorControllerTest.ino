8996fa43fcf7c25f6731c8a7bd0095794a2431e1
Tony Yang <tianyuyang658@yahoo.com>
2018-07-24 05:40:01 -0700

    Add PWM testing for the initial controller and motor test

--- initialMotorControllerTest/initialMotorControllerTest.ino
+++ initialMotorControllerTest/initialMotorControllerTest.ino
@@ -1,13 +1,17 @@
-// Under no load, upload this to an Arduino board attached with a new motor controller
-// Test output voltage with Multimeter to measure the voltage level
-// Ensure new motor board is running good
+/*
+ * New motor controller and motor tester
+ *
+ * 1. Under no load, upload this to an Arduino board attached with a new motor controller
+ * 2. Test output voltage with Multimeter to measure the voltage level
+ *    Ensure new motor board is running good
+ * 3. Attach a new motor to one of the motor output and run the test again
+ */
 
 int enableMotor1 = 5;
 int input1 = 6;
 int input2 = 7;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(enableMotor1, OUTPUT);
   pinMode(input1, OUTPUT);
@@ -15,7 +19,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.println("Enabling Motor 1");
   digitalWrite(enableMotor1, HIGH);
 
@@ -24,6 +27,11 @@ void loop() {
   digitalWrite(input2, LOW);
   delay(3000);
 
+  Serial.println("Stop");
+  digitalWrite(input1, LOW);
+  digitalWrite(input1, LOW);
+  delay(1000);
+
   Serial.println("Backward motion");
   digitalWrite(input1, LOW);
   digitalWrite(input2, HIGH);
@@ -31,5 +39,30 @@ void loop() {
 
   Serial.println("Disabling Motor 1");
   digitalWrite(enableMotor1, LOW);
+  delay(10000);
+
+  Serial.println("Break before PWM testing");
+
+  Serial.println("Forward motion using PWM");
+  analogWrite(enableMotor1, 64);
+  digitalWrite(input1, HIGH);
+  digitalWrite(input2, LOW);
+  delay(3000);
+
+  Serial.println("Stop");
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, LOW);
+  delay(1000);
+
+  Serial.println("Backward motion using PWM");
+  analogWrite(enableMotor1, 64);
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, HIGH);
+  delay(3000);
+
+  Serial.println("Test Complete");
+  digitalWrite(enableMotor1, LOW);
+  digitalWrite(input1, LOW);
+  digitalWrite(input2, LOW);
   delay(20000);
 }
